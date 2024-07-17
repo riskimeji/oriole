53636fcf778cdc79ba0922c4fd30ea8738a62cbb
Riski Meji <meji.riski123@gmail.com>
2024-07-17 21:29:56 +0700

    first commit

---
+++ ori.js
@@ -0,0 +1,86 @@
+const axios = require('axios')
+const { getEmail, cekMail } = require('./temail.js');
+const fs = require('fs');
+
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function regist(){
+    const email = await getEmail();
+    const username = generateRandomUsername();
+
+    const userData = {
+        username: `${email}`,
+        email: `${email}`,
+        password: 'Jelek123@',
+        invitedBy: 'riskimejij4kdmm',
+        name: `${username}`
+    }
+    try {
+        const response = await axios.post('https://api-live.orioleinsights.io/auth/registration', userData);
+        if (response.data.status == 'success') {
+            console.log('Sukses daftar\n');
+            console.log('Saatnya verify OTP\n');
+            const [login, domain] = email.split('@');
+
+            // Tunggu 5 detik sebelum cek email
+            await delay(4000);
+
+            const otp = await cekMail(login, domain);
+            await confirmOtp(email, otp);
+        }
+    } catch (error) {
+        console.log('error');
+    }
+
+}
+
+async function confirmOtp(email, otp){
+    const userData = {
+        username: `${email}`,
+        otp: `${otp}`
+        }
+
+    try {
+        const response = await axios.post('https://api-live.orioleinsights.io/auth/complete-registration', userData);
+        if(response.data.status == 'success'){
+            console.log('berhasil daftar')
+            saveResult(`Berhasil daftar untuk email: ${email}`);
+            await regist()
+        }else{
+            console.log('gagal')
+        }
+    } catch (error) {
+        
+    }
+}
+
+
+function generateRandomUsername() {
+    const characters =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let username = '';
+    for (let i = 0; i < 8; i++) {
+      username += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return username;
+  }
+
+  function saveResult(result) {
+    fs.appendFile('result.txt', `${result}\n`, (err) => {
+        if (err) {
+            console.log('Gagal menyimpan hasil', err);
+        } else {
+            console.log('Hasil berhasil disimpan');
+        }
+    });
+}
+
+
+  regist()
+
+  module.exports = { regist };
---
+++ temail.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const {regist} = require('./ori.js')
+async function getEmail() {
+  try {
+    const response = await axios.get(
+      'https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1'
+    );
+    const data = response.data[0];
+    // const [login, email] = data.split('@');
+    console.log('Email kamu: ', data);
+    // console.log(response);
+    return data;
+    // cekMail(login, email);
+  } catch (error) {
+    console.error(`error on getting email: ${error}`);
+    // return [];
+  }
+}
+
+async function cekMail(login, domain) {
+  try {
+    const response = await axios.get(
+      `https://www.1secmail.com/api/v1/?action=getMessages&login=${login}&domain=${domain}`
+    );
+    const data = response.data;
+    const lastMessage = data[data.length - 1];
+    const id = lastMessage.id;
+    return getInbox(id, login, domain);
+  } catch (error) {
+    console.error(`gada otp peler`);
+    cekMail(login, domain);
+    // return regist;
+    // console.error(`error on getting message: ${error}`);
+  }
+}
+
+async function getInbox(id, login, domain) {
+  try {
+    const response = await axios.get(
+      `https://www.1secmail.com/api/v1/?action=readMessage&login=${login}&domain=${domain}&id=${id}`
+    );
+    const text = response.data.htmlBody;
+      
+    const match = text.match(/confirmation_code=(\d+)/);
+
+    if (match) {
+      const code = match[1];
+      console.log(code);
+      return code;
+    } else {
+      console.log('Kode tidak ditemukan');
+      return 'kode tidak ditemukan';
+    }
+  } catch (error) {
+    console.error(`error on getting message: ${error}`);
+    throw error;
+  }
+}
+
+module.exports = { getEmail, cekMail };
